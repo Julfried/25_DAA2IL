db950a55f7381a77f10d052ebdf9eae3a69909de
Julfried <julian.grimm8@gmail.com>
2026-05-03 10:12:17 +0200

    Add R² value in linregress demo

--- Unit7/linear_regression_demo.py
+++ Unit7/linear_regression_demo.py
@@ -1,7 +1,13 @@
 import pandas as pd
 from scipy import stats
 import matplotlib.pylab as plt
-import numpy as np
+from typing import Protocol, cast
+
+
+class LinregressResultLike(Protocol):
+	slope: float
+	intercept: float
+	rvalue: float
 
 # Load the data
 df = pd.read_csv('./data/brfss.csv')
@@ -9,7 +15,10 @@ df.set_index('idx')
 df = df.dropna()
 
 # Perform linear regression using linear least square
-k, d, p, _, _= stats.linregress(df.htm3, df.wtkg2)
+result = stats.linregress(df.htm3, df.wtkg2)
+k = result.slope
+d = result.intercept
+r_squared = result.rvalue ** 2
 
 print("The line fit was successfull")
 
@@ -20,8 +29,8 @@ x, y = df.htm3, df.wtkg2
 # TODO: Explain this line better
 y_model = k * x + d
 
-# PLot the data and the fitted line
+# Plot the data and the fitted line
 plt.plot(x,y, 'o',label='original data')
-plt.plot(x, y_model, 'r', label='fitted line')
+plt.plot(x, y_model, 'r', label=f'fitted line (R^2={r_squared:.3f})')
 plt.legend()
 plt.show()
